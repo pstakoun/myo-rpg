39f082fe48a43ce24b329572ed50b6bc31dd124d
Stephen Zhao <mail@zhaostephen.com>
2015-08-15 11:46:32 -0400

    2nd commit, working gestures

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 var Myo = require('myo');
 
 Myo.connect();
+//Myo.setLockingPolicy('none');
 
 Myo.on('fist', function(){
-    console.log('You punched the guy in the face!');
+    console.log('You punched the guy in the face (fist)!');
     this.vibrate();
 });
 
 
-Myo.on('spread', function(){
-    console.log('You splattered the guy!');
+Myo.on('fingers_spread', function(){
+    console.log('You splattered the guy (fingersSpread)!');
     this.vibrate();
 });
 
 
-Myo.on('leftwave', function(){
-    console.log('You slapped the guy from the right!');
+Myo.on('wave_in', function(){
+    console.log('You slapped the guy from the right(waveIn)!');
     this.vibrate();
 });
 
 
-Myo.on('rightwave', function(){
-    console.log('You slapped the guy from the left!');
+Myo.on('wave_out', function(){
+    console.log('You slapped the guy from the left(waveOut)!');
     this.vibrate();
 });
